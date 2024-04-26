ffa0f9f291a36129276443d740490089f07b1297
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:43:30 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+// Assignment Operators
+let x = 5;
+let y = 10;
+x += y; //x = x=y
+y += x; //y = y+x
+console.log(x);
+let a = 5;
+let b = 10;
+a -= b; //a = a-b
+b -= a; //b = b-a
+console.log(b);
+let c = 2;
+let d = 4;
+c *= d; //c = c*d
+d *= c; //d = d*c
+console.log(d);
+//Another Example 
+let num1 = 2;
+let num2 = 4;
+num1 += num2; //num1 = num1 + num2
+num2 += num1; //num2 = num2 + num1
+console.log(num2);
---
+++ main.ts
@@ -0,0 +1,32 @@
+        // Assignment Operators
+
+   let x:number = 5;
+   let y:number = 10;
+
+   x += y  //x = x=y
+   y += x  //y = y+x
+   console.log(x)    
+
+
+   let a:number = 5;
+   let b:number = 10;
+
+   a -= b  //a = a-b
+   b -= a  //b = b-a
+   console.log(b)    
+
+   let c:number = 2;
+   let d:number = 4;
+    
+   c *= d   //c = c*d
+   d *= c   //d = d*c
+   console.log(d) 
+
+   //Another Example 
+
+   let num1:number = 2;
+   let num2:number = 4;
+
+    num1 += num2 //num1 = num1 + num2
+    num2 += num1 //num2 = num2 + num1
+    console.log(num2)
